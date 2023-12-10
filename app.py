45c8f85d5698bea3fa8940ac5979ba87a4931e78
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-12-10 19:11:22 +0700

    add docs description, add main, rename animal_classes, change single img (not work in my local) to batch

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
+import uvicorn
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 import tensorflow as tf
 from tensorflow import keras
 from PIL import Image
 import numpy as np
+from tensorflow.keras.preprocessing.image import img_to_array
 
-app = FastAPI()
+app = app = FastAPI(
+        title="Animalink Model Deployment",
+        description="Documentation of Model Apis for AnimaLink Corp",
+        version="1.0",
+        contact={
+            "name": "Our Github Repository",
+            "url": "https://github.com/AnimaLink",
+        },
+        license_info={
+            "name": "MIT License",
+            "url": "https://github.com/AnimaLink/backend-api/blob/main/LICENSE",
+        },
+    )
 model_path = 'animalink.h5'
 model = keras.models.load_model(model_path)
 
-dic = {0 : 'Anjing ajag',  
+animal_classes = {0 : 'Anjing ajag',  
        1 : 'Merak biru',
        2 : 'Merak hijau', 
        3 : 'Sanca hijau',
@@ -28,6 +42,7 @@ def preprocess_image(image):
     img_array = np.asarray(img)
     img_array = img_to_array(img)
     img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0) 
     return img_array
 
 @app.post("/predict")
@@ -46,3 +61,6 @@ async def predict(file: UploadFile = File(...)):
         return JSONResponse(content={"predicted_animal": predicted_animal, "message": message})
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0',port=8080)
