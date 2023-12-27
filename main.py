50afda3c7ab16fa95f46232626344f865f51932f
Ong Riverine <53504975+Riveong@users.noreply.github.com>
2023-12-27 12:44:39 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import uvicorn
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
@@ -7,19 +8,7 @@ from PIL import Image
 import numpy as np
 from tensorflow.keras.preprocessing.image import img_to_array
 
-app = app = FastAPI(
-        title="Animalink Model Deployment",
-        description="Documentation of Model Apis for AnimaLink Corp",
-        version="1.0",
-        contact={
-            "name": "Our Github Repository",
-            "url": "https://github.com/AnimaLink",
-        },
-        license_info={
-            "name": "MIT License",
-            "url": "https://github.com/AnimaLink/backend-api/blob/main/LICENSE",
-        },
-    )
+app = FastAPI()
 
 model_path = 'animalink_model.h5'
 model = tf.keras.models.load_model(model_path)
@@ -48,6 +37,7 @@ def preprocess_image(image):
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
+    start_time = time.time()
     try:
         image_array = preprocess_image(file)
         predictions = model.predict(image_array)
@@ -72,16 +62,17 @@ async def predict(file: UploadFile = File(...)):
             message = f"The predicted animal is {predicted_animal}. This animal can legally be sold."
             predict = f"{round(confidence, 2)}"
 
-        return JSONResponse(content={"status": status, "predicted_animal": predicted_animal, "animal_status": status_extinct, "message": message, "model_confidence": predict})
-    #except Exception as e:
-        #raise HTTPException(status_code=500, detail=str(e))
+        end_time = time.time()
+        benchmark = end_time - start_time
+        return JSONResponse(content={"status": status, "predicted_animal": predicted_animal, "animal_status": status_extinct, "message": message, "model_confidence": predict, "benchmark": benchmark})
     except Exception as e:
-        status = "Error"
+        end_time = time.time()
+        benchmark = end_time - start_time
+        status = "Fail"
         status_extinct = "unknown"
         message = f"An error occurred: {str(e)}"
         predict = "N/A"
 
-        return JSONResponse(content={"status": status, "predicted_animal": "Unknown Animal", "animal_status": status_extinct, "message": message, "model_confidence": predict}, status_code=500)
-
+        return JSONResponse(content={"status": status, "predicted_animal": "Unknown Animal", "animal_status": status_extinct, "message": message, "model_confidence": predict, "benchmark": benchmark}, status_code=500)
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0',port=8080)
